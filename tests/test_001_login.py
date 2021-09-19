7e7f58e25643d066d52cfe2ccba8f6d1d360d3e9
gofreeka <gabormartha.mail@gmail.com>
2021-09-19 14:25:04 +0200

    yml req txt path

--- tests/test_001_login.py
+++ tests/test_001_login.py
@@ -1,10 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
-import pytest
 import time
 
 opt = Options()
+# opt.headless = False
 opt.headless = True
 
 driver = webdriver.Chrome(ChromeDriverManager().install(), options=opt)
@@ -20,7 +20,7 @@ input_password = driver.find_element_by_id("password")
 bt_login = driver.find_element_by_id("login-button")
 
 # Test data
-test_data_tc01 = ['standard_user', 'secret_sauce']
+test_data_tc001 = ['standard_user', 'secret_sauce']
 
 # input elemek kiuritese
 
@@ -30,20 +30,27 @@ def clear_inputs():
     input_password.clear()
 
 
-def test_tc01():
+def check_cookies():
+    # cookies = driver.get_cookies()
+    # for coo in cookies:
+    #     print(coo)
+    print(driver.get_cookie("session-username"))
+
+
+def test_tc001():
     time.sleep(1)
-    input_username.send_keys(test_data_tc01[0])
+    # Check Username and Password field is empty
+    assert input_username.get_attribute("value") == ''
+    assert input_password.get_attribute("value") == ''
+
+    input_username.send_keys(test_data_tc001[0])
     time.sleep(1)
-    input_password.send_keys(test_data_tc01[1])
+    input_password.send_keys(test_data_tc001[1])
     time.sleep(1)
     bt_login.click()
 
-    def check_cookies():
-        # cookies = driver.get_cookies()
-        # for coo in cookies:
-        #     print(coo)
-        print(driver.get_cookie("session-username"))
-
     check_cookies()
 
+
+# test_tc001()
 
